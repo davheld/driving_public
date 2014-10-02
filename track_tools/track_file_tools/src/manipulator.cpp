4c4f0db4b753ba31443313b9c3139ecf3ede3f97
Brice Rebsamen <brice.rebsamen@gmail.com>
2014-10-01 17:50:20 -0700

    a bit of progress on track_file_tools/manipulator

--- track_tools/track_file_tools/src/manipulator.cpp
+++ track_tools/track_file_tools/src/manipulator.cpp
@@ -43,10 +43,15 @@
   */
 
 
+#include <vector>
+#include <string>
+#include <exception>
+
+#include <boost/foreach.hpp>
 #include <boost/program_options.hpp>
 #include <boost/exception/diagnostic_information.hpp>
-
-#include <ros/console.h>
+#include <boost/filesystem.hpp>
+#include <boost/algorithm/string/predicate.hpp>
 
 #include <track_file_io/Tracks.h>
 #include <track_file_io/track_file_io.h>
@@ -75,17 +80,27 @@ std::string help(const boost::program_options::options_description &opts_desc)
   ss <<"in the resulting track file." <<std::endl;
 }
 
+
+// global variables
 track_file_io::Tracks itracks, otracks;
+
+
+// forward declaration
+void process(const std::vector<std::string> &cmds);
+void process(const std::string &cmd);
+void checkCmd(char c);
+int getTrackNb(const std::string &cmd, int pos, int &val);
 
 
 int main(int argc, char **argv)
 {
   namespace bpo = boost::program_options;
   bpo::options_description opts_desc("Allowed options");
-  opts_desc.add_options()("help,h", "produce help message");
+  opts_desc.add_options()
+      ("help,h", "produce help message")
+      ("args", bpo::value< std::vector<std::string> >(), "the list of files and commands to process");
   bpo::positional_options_description pos_opts_desc;
-  pos_opts_desc.add("trk_file", 1);
-  pos_opts_desc.add("track_ops", -1);
+  pos_opts_desc.add("args", -1);
   bpo::variables_map opts;
 
   try {
@@ -97,10 +112,76 @@ int main(int argc, char **argv)
     bpo::notify(opts);
   }
   catch( std::exception & e ) {
-    ROS_FATAL_STREAM(boost::diagnostic_information(e));
+    std::cerr << boost::diagnostic_information(e) << std::endl;
     std::cerr << help(opts_desc) << std::endl;
     return 1;
   }
 
+  std::string trk_filename;
+  std::vector<std::string> cmds;
+
+  BOOST_FOREACH(std::string arg, opts["args"].as< std::vector<std::string> >()) {
+    if( boost::algorithm::ends_with(arg, ".trk") && boost::filesystem3::exists(arg) ) {
+      if( !trk_filename.empty() ) {
+        std::cerr <<"Only accepting one trk file for the moment." <<std::endl;
+        return 1;
+      }
+      trk_filename = arg;
+      continue;
+    }
+    else {
+      cmds.push_back(arg);
+    }
+  }
+
+  //track_file_io::load(trk_filename, itracks);
+
+  try {
+    process(cmds);
+  }
+  catch( std::exception &e ) {
+    std::cerr <<"Error: " <<e.what() <<std::endl;
+    return 1;
+  }
+
   return 0;
+}
+
+
+
+void process(const std::vector<std::string> &cmds)
+{
+  BOOST_FOREACH(const std::string &cmd, cmds) {
+    process(cmd);
+  }
+}
+
+void process(const std::string &cmd)
+{
+  typedef std::pair<char,int> Cmd;
+  std::vector<Cmd> cmds;
+  for(unsigned i=0; i<cmd.size(); ) {
+    checkCmd(cmd[i]);
+    Cmd c;
+    c.first = cmd[i];
+    i = getTrackNb(cmd, i+1, c.second);
+    std::cout <<c.first <<" " <<c.second <<std::endl;
+  }
+}
+
+void checkCmd(char c)
+{
+  static const std::string cmd_codes("smd");
+  if( cmd_codes.find(c)==std::string::npos )
+    throw std::runtime_error( (boost::format("%c is not a recognized command") % c).str() );
+}
+
+int getTrackNb(const std::string &cmd, int pos, int &val)
+{
+  const char *str = cmd.c_str()+pos;
+  char *end = 0;
+  val = strtol(str, &end, 10);
+  if( end==str )
+    throw std::runtime_error( (boost::format("Could not get the track number from \"%s\"") % str).str() );
+  return pos + (end-str);
 }
